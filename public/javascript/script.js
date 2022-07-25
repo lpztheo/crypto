8840b1858b3d1d33cc22cc80775cfbefd75293e8
rkw <tshield09@gmail.com>
2022-07-25 15:36:16 +0200

    ajout image crytpo

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,11 +1,12 @@
-/*/fetch('https://api.thecatapi.com/v1/images/search')
+/*fetch('https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest')
 .then(res =>console.log(res))*/
 
 const apikey = {
   key: '0a1b7ccf-02cb-4010-8423-e744d4491a81'
 }
-  
-request('GET','https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest?CMC_PRO_API_KEY=' + apikey.key)
+
+const proxy = 'https://cors-anywhere.herokuapp.com/';
+request('GET','https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=10&convert=USD&CMC_PRO_API_KEY=' + apikey.key)
 .then((r1) => {
   let x1 = JSON.parse(r1.target.responseText);
   console.log(x1.data.quote.USD.total_market_cap);
@@ -16,7 +17,7 @@ request('GET','https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest
 function request(method, url) {
       return new Promise(function (resolve, reject) {
           var xhr = new XMLHttpRequest();
-          xhr.open(method, url);
+          xhr.open(method,url);
           xhr.setRequestHeader( 'Access-Control-Allow-Origin', '*');
           xhr.setRequestHeader( 'Content-Type', 'application/json' );
           xhr.onload = resolve;
